e65a7767b6f6b3341127415cf58e957c05976d34
sofianeselaoui <sofianeselaoui@gmail.com>
2019-06-15 16:32:24 -0400

    done with arguments

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 options=-Wall -pedantic -std=c11
-
-.PHONY : data clean default
+CP = $(shell cat cp.txt)
+.PHONY : data clean default test 
 
 default : tp1
 
@@ -14,11 +14,18 @@ clean :
 	rm -f *.o tp1
 	rm -f data/*
 
+test : tp1
+	./tp1 -d -k 2 -c $(CP) -i nom_du_fichier_en_entree.ext > fichier_sortie.ext
+	./tp1 -k 1 -e -c $(CP)
+	./tp1 -c $(CP) -d -k 3 -o fichier.out
+	./tp1 -c $(CP) -i nom_du_fichier_en_entree.ext -o fichier_sortie.ext -k 7 -e
+	./tp1 -c $(CP) -d -k 9 < nom_du_fichier_en_entree.ext > fichier_sortie.ext
+	./tp1 -c $(CP) -d -k -1 -a ./data/
+
 data :
 	curl https://www.github.com/guyfrancoeur/INF3135_E2019_TP/raw/master/crypto-data.zip -sLO -o crypto-data.zip
 	if [ ! -d "data" ]; then mkdir data; fi
 	mv crypto-data.zip data/
 	unzip data/crypto-data.zip -d data/
-
 
 
--- tp1.c
+++ tp1.c
@@ -14,23 +14,23 @@ typedef struct {
 } Mode;
 
 typedef struct {
-	bool option_choisie;
+	bool present;
 	FILE * fichier;
 } Fichier_Entree;
 
 typedef struct {
-	bool option_choisie;
+	bool present;
 	FILE * fichier;
 } Fichier_Sortie ;
 
 typedef struct {
-	bool option_choisie;
+	bool present;
 	FILE* fichier;
 } Alphabet;
 
 typedef struct {
-	bool option_choisie;
-	double cle;
+	bool present;
+	int cle;
 } Cle;
 
 typedef struct {
@@ -38,18 +38,30 @@ typedef struct {
 	char * code_perm;
 	Cle cle;
 	Mode mode;
+	Alphabet alphabet;
 
 	//optionnel
 	Fichier_Entree fichier_entree;
 	Fichier_Sortie fichier_sortie;
-	Alphabet alphabet;
 
 } Arguments;
 
 void lectureArguments(int argc, char* argv[], Arguments* arguments){
 
+	arguments->mode.present = false;
+	arguments->fichier_entree.present = false;
+	arguments->fichier_sortie.present = false;
+	arguments->alphabet.present = false;
+	arguments->cle.present = false;
+	arguments->fichier_sortie.fichier = NULL;
+	arguments->fichier_entree.fichier = NULL;
+	arguments->alphabet.fichier = NULL;
+	arguments->code_perm = NULL;
+
 	/**
-	* Mettre les arguments dans la structure
+	* Mettre les arguments dans la structure.
+	* La validation des arguments optionnels se fait
+	* directement dans la boucle.
 	**/
 	for (int i = 1 ; i < argc ; i++){
 
@@ -57,25 +69,42 @@ void lectureArguments(int argc, char* argv[], Arguments* arguments){
 			/**
 			* i+1 doit être le code permanent
 			**/
-			arguments->code_perm = argv[i+1]; 
-			i++;
-
+			if(i + 1 < argc){
+				arguments->code_perm = argv[i+1]; 
+				i++;
+			}
+		
 		} else if(strcmp(argv[i], "-i") == 0) {
 			/**
 			* i+1 doit être le fichier source
 			**/
-			arguments->fichier_entree.option_choisie = true;
-			arguments->fichier_entree.fichier = fopen(argv[i+1],"r");
-			i++;
+			arguments->fichier_entree.present = true;
 
+			if(i + 1 < argc){
+				arguments->fichier_entree.fichier = fopen(argv[i+1],"r");
+				i++;
+			} 
+				
+			if(arguments->fichier_entree.fichier == NULL){
+				printf("%s\n", "Erreur à l'ouverture du fichier d'entrée");
+				exit(5);
+			}
+				
 		} else if(strcmp(argv[i], "-o") == 0) {
 			/**
 			* i+1 doit être le fichier de sortie
 			**/
-			arguments->fichier_sortie.option_choisie = true;
-			arguments->fichier_sortie.fichier = fopen(argv[i+1], "w");
-			i++;
+			arguments->fichier_sortie.present = true;
+			if(i + 1 < argc){
+				arguments->fichier_sortie.fichier = fopen(argv[i+1], "w");
+				i++;
+			} 
 
+			if(arguments->fichier_sortie.fichier == NULL){
+				printf("%s\n", "Erreur à l'ouverture du fichier de sortie");
+				exit(6);
+			}
+			
 		} else if(strcmp(argv[i], "-d") == 0) {
 			/**
 			* decodage
@@ -90,78 +119,134 @@ void lectureArguments(int argc, char* argv[], Arguments* arguments){
 			arguments->mode.present = true;
  			arguments->mode.action = ENCRYPT;
 
-		} else if(strcmp(argv[i], "-k")) {
+		} else if(strcmp(argv[i], "-k") == 0) {
 			/**
 			* i+1 doit être la clé simple
 			**/
-			arguments->cle.option_choisie = true;
-			arguments->cle.cle = atof(argv[i+1]);
-			i++;
+			arguments->cle.present = true;
 
-		} else if (strcmp(argv[i], "-a")) {
+			if(i+1 < argc) {
+				/**
+				* Vérifier que la clé est un nombre
+				**/
+				char* ptr = argv[i+1];
+				for(int j = 0 ; j < strlen(argv[i+1]); j++){
+					if (!isdigit(ptr[j])){
+						arguments->cle.present = false;
+						break;
+					} 
+				}
+
+				if(arguments->cle.present){
+					arguments->cle.cle = atoi(argv[i+1]);
+				}
+				i++;
+			} 
+
+		} else if (strcmp(argv[i], "-a") == 0) {
 			/**
 			* i+1 doit être chemin vers alphabet
 			**/
 
-			arguments->alphabet.option_choisie = true;
-			char* chemin = malloc( strlen(argv[i+1]) + strlen("alphabet.txt") + 1);
-			strcpy(chemin,argv[i+1]);
-			strcat(chemin, "alphabet.txt");
+			arguments->alphabet.present = true;
 
-			arguments->alphabet.fichier = fopen(chemin, "r");
+			if(i + 1 < argc) {
+				char* chemin = malloc( strlen(argv[i+1]) + strlen("alphabet.txt") + 1);
+				strcpy(chemin,argv[i+1]);
+				strcat(chemin, "alphabet.txt");
+				arguments->alphabet.fichier = fopen(chemin, "r");
+				i++;
+			} 
 
-			i++;
-
+			if(arguments->alphabet.fichier == NULL){
+				printf("%s\n", "-A mais erreur à l'ouverture de l'alphabet");
+				exit(8);
+			}
+			
 		} else {
 			/**
 			* Argument non permis
 			**/
+			printf("%s (%s)\n", "Argument non permis", argv[i]);
 			exit(3);
 		}
 	}
 
 	/**
-	* Vérifier que les arguments obligatoires ont bien étés mis
+	* Si option -a non fournie, ouverture de alphabet.txt par défaut
+	**/
+	if(!arguments->alphabet.present) {
+		arguments->alphabet.fichier = fopen("alphabet.txt", "r");
+	}
+
+	/**
+	* Valider les arguments obligatoires.
 	**/
 	if (arguments->code_perm == NULL){
 		//pas de code permanent 
-		fprintf(stderr, "Usage: %s <-c CODEpermanent> <-d | -e> <-k valeur> [-i fichier.in] [-o fichier.out] [-a chemin]\n", argv[0]);
+		printf("Usage: %s <-c CODEpermanent> <-d | -e> <-k valeur> [-i fichier.in] [-o fichier.out] [-a chemin]\n", argv[0]);
 		exit(1);
 	} 
 
-	//code permanent trop court
-	if(strlen(arguments->code_perm) != 12) exit(2);
+	if(strlen(arguments->code_perm) != 12) {
+		//code permanent trop court
+		printf("%s\n", "Code permanent non conforme");
+		exit(2);
+	}
 
-	//pas d'indication encryption ou decryption
-	if (!arguments->mode.present) exit(4);
+	if (!arguments->mode.present){
+		//pas d'indication encryption ou decryption
+		printf("%s\n", "Pas d'indication encryption ou decryption");
+		exit(4);
+	} 
 
-	//erreur à l'ouverture du fichier d'entrée
-	if (arguments->fichier_entree.option_choisie && arguments->fichier_entree.fichier == NULL) exit(5);
+	if(!arguments->cle.present) {
+		//clé non valide ou non fournie
+		printf("%s\n", "Clé non valide ou non fournie");
+		exit(7);
+	}
 
-	//erreur à l'ouverture du fichier de sortie
-	if (arguments->fichier_sortie.option_choisie && arguments->fichier_sortie.fichier == NULL) exit(6);
-
-	//clé non valide ou non fournie
-	if(!arguments->cle.option_choisie || !isdigit(arguments->cle.cle)) exit(7);
-
-	//option -a fournie mais erreur à l'ouverture du fichier alphabet
-	if(arguments->alphabet.option_choisie && arguments->alphabet.fichier == NULL) exit(8);
+	if(arguments->alphabet.fichier == NULL){
+		printf("%s\n", "Erreur à l'ouverture de l'alphabet");
+		exit(8);
+	}
 	
 }
-
-void decryptage(Arguments* arguments){
-
-}
-
-void encryptage(Arguments* arguments){
-
-}
-
 
 int main(int argc, char ** argv){
 
 	Arguments arguments; 
 	lectureArguments(argc, argv, &arguments);
+
+	if(arguments.fichier_entree.present){
+
+		//lire le fichier
+
+		if(arguments.fichier_sortie.present){
+
+			//ecrire dans le fichier
+
+		} else {
+
+			//ecrire dans stdout
+
+		}
+
+	} else {
+
+		//lire stdin
+
+		if(arguments.fichier_sortie.present){
+
+			//ecrire dans le fichier
+
+		} else {
+
+			//ecrire dans stdout
+
+		}
+
+	}
 
 	return 0;
 }
